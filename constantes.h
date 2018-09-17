1268ec0fa2b2fbb1159d04693247613c5c975fc9
nannaparra <43254886+nannaparra@users.noreply.github.com>
2018-09-17 14:47:40 -0300

    Add files via upload
    Subo constantes.h

---
+++ constantes.h
@@ -0,0 +1,14 @@
+#ifndef CONST_H //Comienza del bloque CONST_H
+#define CONST_H
+
+#define FALSE 0
+#define TRUE 1
+#define LST_NO_INI 2
+#define LST_VAC 3
+#define CCP_NO_INI 4
+#define POS_NULA NULL
+#define ELE_NULO NULL
+
+#endif //Termina el bloque CONST_H
+
+
